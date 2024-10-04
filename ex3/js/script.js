fdeecddde1f0e23bb7852b6eb3bb159e91188074
Cássio <cassiomachado1995@outlook.com>
2024-10-04 16:51:37 -0300

    up

--- ex3/js/script.js
+++ ex3/js/script.js
@@ -1,36 +1,32 @@
 function contar() {
-    let txt1 = document.querySelector('#txtn1')
-    let txt2 = document.querySelector('#txtn2')
-    let txt3 = document.querySelector('#txtn3')
-    let res = document.querySelector('#res')
+    let ini = document.querySelector('#txti')
+    let fim = document.querySelector('#txtf')
+    let passo = document.querySelector('#txtp')
 
-
-    inicio = Number(txt1.value)
-    fim = Number(txt2.value)
-    passo = Number(txt3.value)
-    res.style.whitespace = 'nowrap'
-
-    if (txt1 == '' || txt2 == '' || txt3 == '') {
-        alert('[Error]Verifique os dados e tente novamente!')
-        res.innerHTML = `Faltou colocar algum valor :(</p`
+    if (ini.value.length == 0 || fim.value.length == 0 | passo.value.length == 0) {
+        res.innerHTML = 'Impossivel Contar'
     } else {
-        res.innerHTML = '' // limpar o valor 
-
-        // verificação
-        if (passo <= 0) {
-            alert('Passo invalido, Considerado passo 1.')
-            passo = 1
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo inválido! Considerando o PASSO 1')
+            p = 1
         }
+        // contagem crescente
+        if (i < f) {
 
-        let cont = 1
-
-        for (let i = inicio; i <= fim; i += passo) {
-            res.innerHTML += `${i} \u{1F449} `
-            cont++
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+            res.innerHTML += `\u{1F3C1}`
+        } else {
+            // contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
-
-        res.innerHTML += `\u{1F3C1} Chegamos ao fim!`
-
+        res.innerHTML += `\u{1F3C1}`
     }
-
 }
