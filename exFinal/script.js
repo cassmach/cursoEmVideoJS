db4ce86439ba8c88940e83ee9adf51503c39af69
Cássio <cassiomachado1995@outlook.com>
2024-10-09 10:54:31 -0300

    up

--- exFinal/script.js
+++ exFinal/script.js
@@ -1,27 +1,49 @@
 let num = document.querySelector('#txtn')
 let viewResult = document.querySelector('#seltab')
-viewResult.innerHTML = ''
+let resultado = document.querySelector('#resultado');
+viewResult.innerHTML = '';
 let valores = [];
 
-
 function adicionar() {
-    
+
     if (num.value.length == 0) {
         alert('Adicione os números')
     } else {
         let n = Number(num.value);
-        
-            
-            let item = document.createElement('option')
-            item.text = `Valor ${n} adicionado.`
-            viewResult.appendChild(item)
-           
-            valores.push(n)
+
+
+        let item = document.createElement('option')
+        item.text = `Valor ${n} adicionado.`
+        viewResult.appendChild(item)
+
+        valores.push(n)
     }
 }
 
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Nenhum valor foi adicionado !')
+    } else {
+        let soma = 0;
+        console.log(valores)
 
-function finalizar () {
-   console.log(valores)
+        for (let i = 0; i < valores.length; i++) {
+            soma += valores[i]
+
+        }
+        let maiorValor = Math.max(...valores)
+        let menorValor = Math.min(...valores)
+        let mediaValor = soma / valores.length
+
+
+
+        resultado.innerHTML = `
+        <p>A soma dos valores é ${soma}</p>
+        <p>O maior valor foi ${maiorValor}</p>
+        <p>O menor valor foi ${menorValor}</p>
+        <p>A media dos valores foi ${mediaValor.toFixed(2)}</p>
+        `
+
+    }
 }
 
